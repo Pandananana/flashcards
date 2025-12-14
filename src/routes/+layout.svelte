666cbc202e02a6ae5d79d62983edc19bca09932d
Andreas Wendelboe <andreaswendelboe.dk@gmail.com>
2025-12-14 12:36:36 +0100

    Update favicon implementation and add various icon sizes for better support across devices

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
 	import "./layout.css";
-	import favicon from "$lib/assets/favicon.ico";
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head><link rel="icon" href="/favicon.ico" /></svelte:head>
 {@render children()}
